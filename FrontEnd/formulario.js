5eddf3aab9b3d3c3940d37b47025777dfd87cdda
Gabi-Aguirre <aguirregabriel.unpsjb@gmail.com>
2023-05-22 18:43:59 -0300

    Formulario relizado y detalles cambiados

--- FrontEnd/formulario.js
+++ FrontEnd/formulario.js
@@ -1,30 +1,72 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar envío del formulario
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-    var emailInput = document.getElementById('email');
-    var passwordInput = document.getElementById('password');
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-    var isValid = true;
+    validateInputs();
+});
 
-    // Validación del email
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(emailInput.value)) {
-      isValid = false;
-      emailInput.classList.add('error');
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
     } else {
-      emailInput.classList.remove('error');
+        setSuccess(username);
     }
 
-    // Validación de la contraseña
-    if (passwordInput.value.trim() === '') {
-      isValid = false;
-      passwordInput.classList.add('error');
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
     } else {
-      passwordInput.classList.remove('error');
+        setSuccess(email);
     }
 
-    if (isValid) {
-      // Realizar alguna acción, como enviar el formulario
-      console.log('Formulario válido. Enviar datos al servidor...');
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
     }
-  });
+
+    if(password2Value === '') {
+        setError(password2, 'Por favor confirme su contraseña');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "La contraseña no coincide");
+    } else {
+        setSuccess(password2);
+    }
+
+};
