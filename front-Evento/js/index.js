444d9674a5b5d75440da7c96b507da3e68d32149
Carlos-Bustamante <cmontanobustamante@frba.utn.edu.ar>
2023-07-13 18:13:59 -0300

    TPO end

---
+++ front-Evento/js/index.js
@@ -0,0 +1,26 @@
+export default {
+    data() {
+      return {
+        integrantes: [
+          {
+            id: 1,
+            nombre: 'Integrante 1',
+            descripcion: 'Descripción del integrante 1.',
+            edad: 25
+          },
+          {
+            id: 2,
+            nombre: 'Integrante 2',
+            descripcion: 'Descripción del integrante 2.',
+            edad: 30
+          },
+          {
+            id: 3,
+            nombre: 'Integrante 3',
+            descripcion: 'Descripción del integrante 3.',
+            edad: 28
+          }
+        ]
+      };
+    }
+  };
